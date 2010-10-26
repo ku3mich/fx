1d06ada7a18722675e60daedb6d8e002a7958f98
sergey.glukhov <sergey.glukhov@gmail.com>
2010-10-26 14:17:00 +0400

    add EnumValueAttribute class + some usefull enum extensions

---
+++ sources/AnjLab.FX/Enums/EnumExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AnjLab.FX.Enums
+{
+    public static class EnumExtensions
+    {
+        private static readonly Dictionary<Type, Dictionary<object, string>> enumDescriptions = new Dictionary<Type, Dictionary<object, string>>();
+
+        public static IList<KeyValuePair<object, string>> GetDescriptions(this Enum source)
+        {
+            var result = new List<KeyValuePair<object, string>>();
+            Type dataType = source.GetType();
+            foreach (var value in Enum.GetValues(dataType))
+            {
+                FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+
+                var attrs = fieldInfo.GetCustomAttributes(typeof(EnumValueAttribute), false) as EnumValueAttribute[];
+                if (attrs != null && attrs.Length > 0)
+                    result.Add(new KeyValuePair<object, string>(value, attrs.First().Description));
+                else
+                    result.Add(new KeyValuePair<object, string>(value, value.ToString()));
+            }
+            return result;
+        }
+
+        public static string ToValueString(this Enum enumValue)
+        {
+            var type = enumValue.GetType();
+            if (!enumDescriptions.ContainsKey(type))
+                enumDescriptions.Add(type, enumValue.ToDictionary<object>());
+            return enumDescriptions[type][enumValue];
+        }
+
+        public static Dictionary<T, string> ToDictionary<T>(this Enum source)
+        {
+            return source.GetDescriptions().ToDictionary(keyValue => (T)keyValue.Key, keyValue => keyValue.Value);
+        }
+    }
+}
---
+++ sources/AnjLab.FX/Enums/EnumValueAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AnjLab.FX.Enums
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class EnumValueAttribute : Attribute
+    {
+        public EnumValueAttribute(string description)
+        {
+            Description = description;
+        }
+
+        public string Description { get; set; }
+    }
+}
