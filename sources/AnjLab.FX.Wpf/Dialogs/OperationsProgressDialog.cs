134d85ca1ffe08ed56e14c4ac8df64836f4b0468
sergey.glukhov <sergey.glukhov@gmail.com>
2011-03-01 16:49:47 +0300

    ProgressDialog for WPF

---
+++ sources/AnjLab.FX.Wpf/Dialogs/OperationsProgressDialog.cs
@@ -0,0 +1,348 @@
+﻿using AnjLab.FX.Wpf.Dialogs.Interop;
+using System;
+using System.Runtime.InteropServices;
+
+namespace AnjLab.FX.Wpf.Dialogs
+{
+    /// <summary>Describes an action being performed.</summary>
+	public enum OperationAction : uint
+	{
+		/// <summary>No action is being performed.</summary>
+		None = 0,
+		/// <summary>Files are being moved.</summary>
+		Moving = (None + 1),
+		/// <summary>Files are being copied.</summary>
+		Copying = (Moving + 1),
+		/// <summary>Files are being deleted.</summary>
+		Recycling = (Copying + 1),
+		/// <summary>A set of attributes are being applied to files.</summary>
+		ApplyingAttributes = (Recycling + 1),
+		/// <summary>A file is being downloaded from a remote source.</summary>
+		Downloading = (ApplyingAttributes + 1),
+		/// <summary>An Internet search is being performed.</summary>
+		SearchingInternet = (Downloading + 1),
+		/// <summary>A calculation is being performed.</summary>
+		Calculating = (SearchingInternet + 1),
+		/// <summary>A file is being uploaded to a remote source.</summary>
+		Uploading = (Calculating + 1),
+		/// <summary>A local search is being performed.</summary>
+		SearchingFiles = (Uploading + 1),
+		/// <summary>A deletion is being performed.</summary>
+		Deleting = (SearchingFiles + 1),
+		/// <summary>A renaming action is being performed.</summary>
+		Renaming = (Deleting + 1),
+		/// <summary>A formatting action is being performed.</summary>
+		Formatting = (Renaming + 1)
+	}
+
+	/// <summary>Indicates opeartions mode.</summary>
+	public enum OperationMode : uint
+	{
+		None = 0x0,
+		/// <summary>Indicates operation is running.</summary>
+		Run = 0x1,
+		/// <summary>Indicates pre-flight mode, calculating operation time.</summary>
+		Preflight = 0x2,
+		/// <summary>Indicates operation is rolling back, undo has been selected.</summary>
+		Undoing = 0x4,
+		/// <summary>Indicates error dialogs are blocking progress from completing.</summary>
+		ErrorsBlocking = 0x8,
+		/// <summary>Indicates length of the operation is indeterminate.</summary
+		/// <remarks>Don't show a timer, progressbar is in marquee mode.</remarks>
+		Indeterminate = 0x10
+	}
+
+	/// <summary>Provides operation status values.</summary>
+	public enum OperationStatus : uint
+	{
+		/// <summary>Operation is running, no user intervention.</summary>
+		Running = 0x1,
+		/// <summary>Operation has been paused by the user.</summary>
+		Paused = 0x2,
+		/// <summary>Operation has been canceled by the user - now go undo.</summary>
+		Cancelled = 0x3,
+		/// <summary>Operation has been stopped by the user - terminate completely.</summary>
+		Stopped = 0x4,
+		/// <summary>Operation has gone as far as it can go without throwing error dialogs.</summary>
+		Errors = 0x5
+	}
+
+	/// <summary>Displays a progress dialog box that shows an action being performed on items.</summary>
+	public class OperationsProgressDialog : ProgressDialog
+	{
+		private readonly IOperationsProgressDialog instance;
+		private OperationAction action;
+		private OperationMode mode;
+		private ulong size;
+		private ulong totalSize;
+		private ulong count;
+		private ulong totalCount;
+		private string sourcePath;
+		private string targetPath;
+
+		public OperationsProgressDialog()
+			: base()
+		{
+			instance = (IOperationsProgressDialog)coclassObject;
+		}
+
+		/// <summary>Gets or sets which progress dialog operation is occuring.</summary>
+		public OperationAction Action
+		{
+			get
+			{
+				return action;
+			}
+			set
+			{
+				action = value;
+				if (IsVisible)
+					instance.SetOperation(action);
+			}
+		}
+
+		/// <summary>Gets or sets progress dialog operations mode.</summary>
+		public OperationMode Mode
+		{
+			get
+			{
+				return mode;
+			}
+			set
+			{
+				mode = value;
+				if (IsVisible)
+					instance.SetMode(mode);
+			}
+		}
+
+		/// <summary>Gets operation status for progress dialog.</summary>
+		public OperationStatus Status
+		{
+			get
+			{
+				OperationStatus value;
+				instance.GetOperationStatus(out value);
+				return value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets a value that indicates the operation
+		/// can be paused and adds a pause button.
+		/// </summary>
+		public bool HasPauseButton
+		{
+			get { return GetOption((uint)PROGDLG.OPPROGDLG_ENABLEPAUSE); }
+			set { SetOption((uint)PROGDLG.OPPROGDLG_ENABLEPAUSE, value); }
+		}
+
+		/// <summary>
+		/// Gets or sets a value that indicates the operation
+		/// can be undone in the dialog or not.
+		/// </summary>
+		public bool AllowsUndo
+		{
+			get { return GetOption((uint)PROGDLG.OPPROGDLG_ALLOWUNDO); }
+			set { SetOption((uint)PROGDLG.OPPROGDLG_ALLOWUNDO, value); }
+		}
+
+		/// <summary>
+		/// Gets or sets the source shell item path or parsing name
+		/// to generate from/to display for the progress dialog.
+		/// </summary>
+		public string SourcePath
+		{
+			get
+			{
+				return sourcePath;
+			}
+			set
+			{
+				sourcePath = value;
+				UpdateLocations();
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the target shell item path or parsing name
+		/// to generate from/to display for the progress dialog.
+		/// </summary>
+		public string TargetPath
+		{
+			get
+			{
+				return targetPath;
+			}
+			set
+			{
+				targetPath = value;
+				UpdateLocations();
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets a value that indicates whether the path
+		/// of source item should be displayed in the progress dialog.
+		/// </summary>
+		public bool DisplaysSourcePath
+		{
+			get { return !GetOption((uint)PROGDLG.OPPROGDLG_DONTDISPLAYSOURCEPATH); }
+			set { SetOption((uint)PROGDLG.OPPROGDLG_DONTDISPLAYSOURCEPATH, !value); }
+		}
+
+		/// <summary>
+		/// Gets or sets a value that indicates whether the path
+		/// of target item should be displayed in the progress dialog.
+		/// </summary>
+		public bool DisplaysTargetPath
+		{
+			get { return !GetOption((uint)PROGDLG.OPPROGDLG_DONTDISPLAYDESTPATH); }
+			set { SetOption((uint)PROGDLG.OPPROGDLG_DONTDISPLAYDESTPATH, !value); }
+		}
+
+		public override bool EstimatesTimeRemaining
+		{
+			get
+			{
+				return true;
+			}
+			set
+			{
+			}
+		}
+
+		public override bool ProgressBarIsMarquee
+		{
+			get
+			{
+				return Mode == OperationMode.Indeterminate;
+			}
+			set
+			{
+				Mode = value ? OperationMode.Indeterminate : OperationMode.Run;
+			}
+		}
+
+		public override bool IsCancelled
+		{
+			get { return Status == OperationStatus.Cancelled; }
+		}
+
+		/// <summary>Gets or sets the current size in bytes, used for showing progress in bytes.</summary>
+		public ulong Size
+		{
+			get
+			{
+				return size;
+			}
+			set
+			{
+				size = value;
+				UpdateProgress();
+			}
+		}
+
+		/// <summary>Gets or sets the total size in bytes, used for showing progress in bytes.</summary>
+		public ulong TotalSize
+		{
+			get
+			{
+				return totalSize;
+			}
+			set
+			{
+				totalSize = value;
+				UpdateProgress();
+			}
+		}
+
+		/// <summary>Gets or sets the number of current items, used for showing progress in items.</summary>
+		public ulong Count
+		{
+			get
+			{
+				return count;
+			}
+			set
+			{
+				count = value;
+				UpdateProgress();
+			}
+		}
+
+		/// <summary>Gets or sets the total number of items, used for showing progress in items.</summary>
+		public ulong TotalCount
+		{
+			get
+			{
+				return totalCount;
+			}
+			set
+			{
+				totalCount = value;
+				UpdateProgress();
+			}
+		}
+
+		public override void Reset()
+		{
+			base.Reset();
+			action = OperationAction.None;
+			mode = OperationMode.None;
+			size = 0ul;
+			totalSize = 0ul;
+			count = 0ul;
+			totalCount = 0ul;
+		}
+
+		protected override void RunDialogImpl(IntPtr hwndOwner)
+		{
+			instance.StartProgressDialog(hwndOwner, (PROGDLG)Options);
+			if (action != OperationAction.None)
+				instance.SetOperation(action);
+			if (mode != OperationMode.None)
+				instance.SetMode(mode);
+			UpdateLocations();
+			UpdateProgress();
+		}
+
+		protected override void UpdateProgress()
+		{
+			if (IsVisible)
+				instance.UpdateProgress(Value, TotalValue, Size, TotalSize, Count, TotalCount);
+		}
+
+		private void UpdateLocations()
+		{
+			if (!IsVisible)
+				return;
+
+			var sourceItem = IntPtr.Zero;
+			var targetItem = IntPtr.Zero;
+			try {
+				var interfaceId = new Guid(IID_IShellItem);
+
+				SHCreateItemFromParsingName(sourcePath, IntPtr.Zero, ref interfaceId, out sourceItem);
+				SHCreateItemFromParsingName(targetPath, IntPtr.Zero, ref interfaceId, out targetItem);
+
+				instance.UpdateLocations(sourceItem, targetItem, IntPtr.Zero);
+			}
+			finally {
+				if (sourceItem != IntPtr.Zero)
+					Marshal.Release(sourceItem);
+				if (targetItem != IntPtr.Zero)
+					Marshal.Release(targetItem);
+			}
+		}
+
+		private const string IID_IShellItem = "43826D1E-E718-42EE-BC55-A1E261C37BFE";
+
+		[DllImport("shell32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+		private static extern uint SHCreateItemFromParsingName(
+				[MarshalAs(UnmanagedType.LPWStr)] string path,
+				IntPtr pbc,
+				ref Guid riid,
+				out IntPtr shellItem);
+	}
+}
---
+++ sources/AnjLab.FX.Wpf/Dialogs/ProgressDialog.cs
@@ -0,0 +1,259 @@
+﻿using AnjLab.FX.Wpf.Dialogs.Interop;
+using System;
+using Microsoft.Win32;
+
+namespace AnjLab.FX.Wpf.Dialogs
+{
+    /// <summary>Displays a progress dialog box.</summary>
+	public class ProgressDialog : CommonDialog
+	{
+		private const uint CaptionLineIndex = 1u;
+		private const uint MessageLineIndex = 2u;
+		private const uint TimeRemainingLineIndex = 3u;
+
+		protected readonly object coclassObject;
+		private readonly IProgressDialog instance;
+		private PROGDLG options;
+		private ulong totalValue;
+		private ulong value;
+		private string title;
+		private string caption;
+		private string message;
+		private string timeRemaining;
+		private string cancelMessage;
+
+		public ProgressDialog()
+			: base()
+		{
+			coclassObject = new Interop.ProgressDialog();
+			instance = (IProgressDialog)coclassObject;
+			Reset();
+		}
+
+		/// <summary>Gets a value indicating whether the progress dialog box is visible on the screen.</summary>
+		public bool IsVisible { get; private set; }
+
+		/// <summary>Gets or sets the title of the progress dialog box.</summary>
+		public string Title
+		{
+			get
+			{
+				return title;
+			}
+			set
+			{
+				title = value;
+				instance.SetTitle(title);
+			}
+		}
+
+		/// <summary>Gets or sets the caption of the progress dialog box.</summary>
+		public string Caption
+		{
+			get
+			{
+				return caption;
+			}
+			set
+			{
+				caption = value;
+				instance.SetLine(CaptionLineIndex, caption, CompactsPaths, IntPtr.Zero);
+			}
+		}
+
+		/// <summary>Gets or sets a message to be displayed during the operation.</summary>
+		public string Message
+		{
+			get
+			{
+				return message;
+			}
+			set
+			{
+				message = value;
+				instance.SetLine(MessageLineIndex, message, CompactsPaths, IntPtr.Zero);
+			}
+		}
+
+		/// <summary>Displays a message or the estimated time remaining.</summary>
+		/// <remarks>The estimated time is displayed if <see cref="EstimatesTimeRemaining"/> is set to <c>true</c>.</remarks>
+		public string TimeRemaining
+		{
+			get
+			{
+				return timeRemaining;
+			}
+			set
+			{
+				timeRemaining = value;
+				instance.SetLine(TimeRemainingLineIndex, timeRemaining, CompactsPaths, IntPtr.Zero);
+			}
+		}
+
+		/// <summary>Gets or sets a message to be displayed if the user cancels the operation.</summary>
+		public string CancelMessage
+		{
+			get
+			{
+				return cancelMessage;
+			}
+			set
+			{
+				cancelMessage = value;
+				instance.SetCancelMsg(cancelMessage, IntPtr.Zero);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets a value that indicates whether path strings
+		/// are compacted if they are too large to fit on a line.
+		/// </summary>
+		public bool CompactsPaths { get; set; }
+
+		/// <summary>Gets or sets the current progress of the operation in points.</summary>
+		public ulong Value
+		{
+			get
+			{
+				return value;
+			}
+			set
+			{
+				this.value = value;
+				UpdateProgress();
+			}
+		}
+
+		/// <summary>Gets or sets the highest possible <see cref="Value"/> of the operation state.</summary>
+		public ulong TotalValue
+		{
+			get
+			{
+				return totalValue;
+			}
+			set
+			{
+				totalValue = value;
+				UpdateProgress();
+			}
+		}
+
+		/// <summary>Gets or sets a value that specifies whether the progress dialog box is modal.</summary>
+		public bool IsModal
+		{
+			get { return GetOption((uint)PROGDLG.PROGDLG_MODAL); }
+			set { SetOption((uint)PROGDLG.PROGDLG_MODAL, value); }
+		}
+
+		/// <summary>Gets or sets a value that indicates whether the dialog box can be minimized.</summary>
+		public bool HasMinimizeButton
+		{
+			get { return !GetOption((uint)PROGDLG.PROGDLG_NOMINIMIZE); }
+			set { SetOption((uint)PROGDLG.PROGDLG_NOMINIMIZE, !value); }
+		}
+
+		/// <summary>Gets or sets a value that indicates whether the "time remaining" text is shown.</summary>
+		public bool ShowsTimeRemaining
+		{
+			get { return !GetOption((uint)PROGDLG.PROGDLG_NOTIME); }
+			set { SetOption((uint)PROGDLG.PROGDLG_NOTIME, !value); }
+		}
+
+		/// <summary>Gets or sets a value that indicates whether the time remaining is estimated.</summary>
+		public virtual bool EstimatesTimeRemaining
+		{
+			get { return GetOption((uint)PROGDLG.PROGDLG_AUTOTIME); }
+			set { SetOption((uint)PROGDLG.PROGDLG_AUTOTIME, value); }
+		}
+
+		/// <summary>Gets or sets a value that indicates whether the progress dialog box has a progress bar.</summary>
+		public bool HasProgressBar
+		{
+			get { return !GetOption((uint)PROGDLG.PROGDLG_NOPROGRESSBAR); }
+			set { SetOption((uint)PROGDLG.PROGDLG_NOPROGRESSBAR, !value); }
+		}
+
+		/// <summary>Gets or sets a value that indicates whether the progress bar is displayed as marquee.</summary>
+		public virtual bool ProgressBarIsMarquee
+		{
+			get { return GetOption((uint)PROGDLG.PROGDLG_MARQUEEPROGRESS); }
+			set { SetOption((uint)PROGDLG.PROGDLG_MARQUEEPROGRESS, value); }
+		}
+
+		/// <summary>Gets or sets a value that indicates whether the user can cancel the operation.</summary>
+		public bool HasCancelButton
+		{
+			get { return !GetOption((uint)PROGDLG.PROGDLG_NOCANCEL); }
+			set { SetOption((uint)PROGDLG.PROGDLG_NOCANCEL, !value); }
+		}
+
+		/// <summary>Checks whether the user has canceled the operation.</summary>
+		public virtual bool IsCancelled
+		{
+			get { return instance.HasUserCancelled(); }
+		}
+
+		protected uint Options
+		{
+			get { return (uint)options; }
+		}
+
+		/// <summary>Resets the properties of the progress dialog to their default values.</summary>
+		public override void Reset()
+		{
+			options = PROGDLG.PROGDLG_NORMAL;
+			totalValue = 0ul;
+			value = 0ul;
+			CompactsPaths = false;
+			Title = String.Empty;
+			Caption = String.Empty;
+			Message = String.Empty;
+			TimeRemaining = String.Empty;
+			CancelMessage = String.Empty;
+		}
+
+		/// <summary>Closes the progress dialog box.</summary>
+		public void CloseDialog()
+		{
+			if (IsVisible) {
+				instance.StopProgressDialog();
+				IsVisible = false;
+			}
+		}
+
+		protected override bool RunDialog(IntPtr hwndOwner)
+		{
+			if (IsVisible)
+				throw new InvalidOperationException();
+
+			IsVisible = true;
+			RunDialogImpl(hwndOwner);
+			return IsVisible;
+		}
+
+		protected virtual void RunDialogImpl(IntPtr hwndOwner)
+		{
+			instance.StartProgressDialog(hwndOwner, null, options, IntPtr.Zero);
+			UpdateProgress();
+		}
+
+		protected bool GetOption(uint option)
+		{
+			return (options & (PROGDLG)option) != PROGDLG.PROGDLG_NORMAL;
+		}
+
+		protected void SetOption(uint option, bool value)
+		{
+			if (value)
+				options |= (PROGDLG)option;
+			else
+				options &= ~((PROGDLG)option);
+		}
+
+		protected virtual void UpdateProgress()
+		{
+			if (IsVisible)
+				instance.SetProgress64(Value, TotalValue);
+		}
+	}
+}
