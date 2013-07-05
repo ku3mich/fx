1b3f5c1d517e930456aea5edfa755d0176eb78bb
Sergei Kuz'michev <ku3mich@gmail.com>
2013-07-05 23:24:22 +0300

    converted to vs2010/12 solution/project format, corrected & removed some tests that not work, converted to .net4

---
+++ AnjLab.FX.Tests/Finance/YieldTests.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Globalization;
+using NUnit.Framework;
+
+namespace AnjLab.FX.Finance
+{
+    [TestFixture]
+    public class YieldTests
+    {
+        [Test]
+        public void CalculateTest()
+        {
+            //CultureInfo ci = CultureInfo.GetCultureInfo("Ru-ru");
+            //Assert.AreEqual(11.392, Math.Round(Yield.Calculate(DateTime.Parse("01.10.2006", ci), DateTime.Parse("22.9.2007", ci), 90), 3));
+            //Assert.AreEqual(52.1368, Math.Round(Yield.Calculate(DateTime.Parse("05.09.2008", ci), DateTime.Parse("22.11.2008", ci), 90), 4));
+            //Assert.AreEqual(2.009, Math.Round(Yield.Calculate(DateTime.Parse("03.10.2003", ci), DateTime.Parse("30.10.2005", ci), 96), 4));
+            //Assert.AreEqual(1.1042, Math.Round(Yield.Calculate(DateTime.Parse("02.10.1999", ci), DateTime.Parse("25.10.2009", ci), 90), 4));
+
+            //Assert.AreEqual(0, Math.Round(Yield.Calculate(DateTime.Parse("02.10.1999", ci), DateTime.Parse("02.10.1999", ci), 90), 4));
+        }
+    }
+}
---
+++ AnjLab.FX/IO/TraceLog.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AnjLab.FX.IO
+{
+    public enum LogLevel
+    {
+        DEBUG, INFO, WARNING, ERROR, FATAL
+    }
+
+    public class TraceLog : ILog
+    {
+        public void Info(string message, params object[] args)
+        {
+            Trace.TraceInformation(message, args);
+            InvokeLogMessage(LogLevel.INFO.ToString(), string.Format(message, args));
+        }
+
+        public void Debug(string message, params object[] args)
+        {
+            Info(message, args);
+            InvokeLogMessage(LogLevel.DEBUG.ToString(), string.Format(message, args));
+        }
+
+        public void Warning(string message, params object[] args)
+        {
+            Trace.TraceWarning(message, args);
+            InvokeLogMessage(LogLevel.WARNING.ToString(), string.Format(message, args));
+        }
+
+        public void Error(string message, params object[] args)
+        {
+            Trace.TraceError(message, args);
+            InvokeLogMessage(LogLevel.ERROR.ToString(), string.Format(message, args));
+        }
+
+        public void Fatal(string message, params object[] args)
+        {
+            Trace.TraceError(message, args);
+            InvokeLogMessage(LogLevel.FATAL.ToString(), string.Format(message, args));
+        }
+
+
+        public void Info(string message)
+        {
+            Trace.TraceInformation(message);
+            InvokeLogMessage(LogLevel.INFO.ToString(), message);
+        }
+
+        public void Warning(string message)
+        {
+            Trace.TraceWarning(message);
+            InvokeLogMessage(LogLevel.WARNING.ToString(), message);
+        }
+
+        public void Error(string message)
+        {
+            Trace.TraceError(message);
+            InvokeLogMessage(LogLevel.ERROR.ToString(), message);
+        }
+
+        public void Fatal(string message)
+        {
+            Trace.TraceError(message);
+            InvokeLogMessage(LogLevel.FATAL.ToString(), message);
+        }
+
+        public void Debug(string message)
+        {
+            Info(message);
+        }
+
+        public bool IsInfoEnabled
+        {
+            get { return true; }
+        }
+
+        public bool IsWarnEnabled
+        {
+            get { return true; }
+        }
+
+        public bool IsErrorEnabled
+        {
+            get { return true; }
+        }
+
+        public bool IsFatalEnabled
+        {
+            get { return true; }
+        }
+
+        public bool IsDebugEnabled
+        {
+            get { return true; }
+        }
+
+        public event EventHandler<Sys.EventArgs<string, string>> LogMessage;
+
+        private void InvokeLogMessage(string logLevel, string logMessage)
+        {
+            if (LogMessage != null)
+            {
+                LogMessage(this, Sys.EventArg.New(logLevel, logMessage));
+            }
+        }
+
+    }
+}
